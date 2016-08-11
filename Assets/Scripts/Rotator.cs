5e7a086ea34566759003b089cc3e0e22d0364fd2
Yukai Li <yukaili.geek@gmail.com>
2016-08-10 18:37:18 -0600

    Add sample scene and content

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    public float Speed = 25;
+    public bool RotateZOnly = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float r = Speed * Time.deltaTime;
+        if (RotateZOnly)
+        {
+            gameObject.transform.Rotate(0, 0, r);
+        }
+        else
+        {
+            gameObject.transform.Rotate(r, r, 0);
+        }
+    }
+}
